46bff6d389a29f0201ae9576be749cf8abcbdf7b
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2025-03-26 14:57:59 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.galeriamvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.galeriamvvm.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    int indeks;
+    private ArrayList<Integer> obrazy;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.lewo.setOnClickListener(view1 -> lewo());
+        binding.prawo.setOnClickListener(view1 -> prawo());
+        obrazy = new ArrayList<Integer>();
+        dodajobrazy();
+    }
+
+    private void dodajobrazy() {
+        obrazy.add(R.drawable.wb);
+        obrazy.add(R.drawable.mire);
+        obrazy.add(R.drawable.wh);
+    }
+
+    private void prawo()
+    {
+        indeks++;
+        if (indeks>obrazy.size()-1)
+        {
+            indeks=0;
+        }
+        wyswietl();
+    }
+    private void lewo()
+    {
+        indeks--;
+        if (indeks<0)
+        {
+            indeks= obrazy.size()-1;
+        }
+        wyswietl();
+    }
+    private void wyswietl() {
+        binding.image.setImageResource(obrazy.get(indeks));
+    }
+}
